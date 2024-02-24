9702f885d19ff6f1de365d7aeccf7e7f70e0ac22
Kara Grassau <hkgrassau@gmail.com>
2024-02-24 13:41:51 -0600

    changed time format

--- data-processing/cleaner.py
+++ data-processing/cleaner.py
@@ -14,7 +14,8 @@ def filter_rows(input_csv_file, output_csv_file):
         # Filter rows and write to output CSV
         for row in csv_reader:
             if len(row) > 1 and row[14].lower() == 'tx':
-                csv_writer.writerow([row[2], row[3], row[5], row[6], row[11]])
+                time_adj = row[3][10:13]
+                csv_writer.writerow([row[2], time_adj, row[5], row[6], row[11]])
 
 input_file_path = r"data-processing\US_Accidents_March23.csv"
 output_file_path = r"data-processing\accident_texas.csv" 
--- data-processing/manipulator.py
+++ data-processing/manipulator.py
@@ -8,9 +8,14 @@ unique_times = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 streets = data['Street']
 unique_street = streets.unique()
 array_of_times_to_streets = []
-for j in range(len(unique_street)):
-  for i in unique_times:
-    severity = data.query(data['Start_Time']==i and data['Street']==j)["Severity"]
-    array_of_times_to_streets.append(0)
-    for k in range(len(severity)):
-      array_of_times_to_streets[i+j] += severity[k]
+counter = 0
+for j in unique_street:
+    for i in unique_times:
+        severity = data.query("(Start_Time == @i) & (Street == @j)")["Severity"]
+        print(i,j)
+        print(severity)
+        array_of_times_to_streets.append(0)
+        counter += 1
+        for k in range(severity.shape[0]):
+            array_of_times_to_streets[counter-1] += severity(k)
+            print(severity.get(k))
