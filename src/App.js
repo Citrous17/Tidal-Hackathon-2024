65369caaca351315ac40b0430045fc79456d0f82
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2024-02-24 11:46:42 -0600

    Lets get this bread

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LETS GET THIS BREAD
         </a>
       </header>
     </div>
